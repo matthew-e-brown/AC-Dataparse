a4101935181be6067e53a944a24ec35c63e3c6c2
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-04-01 22:40:54 -0400

    Implement ROMFS extraction
    - As a result, now supports automatically pulling all languages
    - Uses AboodXD's Sarc Tool and Facebook's ZSTD

--- Dataparse/parse.py
+++ Dataparse/parse.py
@@ -12,24 +12,31 @@ def export(data, filename):
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 
-all_styles = msbp.parse_file(f"{cwd}/Extracted/App.msbp", verbose=True)
+all_styles = msbp.parse_file(f"{cwd}/extracted/App.msbp", verbose=True)
 fact_messages = {}
 item_messages = {}
 
-# Extract the comments:
-for name in ['Fish', 'Insect', 'Fossil']:
-  fact_messages[name] = msbt.parse_file(f"{cwd}/Extracted/SP_owl_Comment_{name}.msbt", verbose=True)
+for lang in [ f for f in os.listdir(f"{cwd}/extracted") if os.path.isdir(f"{cwd}/extracted/{f}") ]:
+  print(f"Parsing {lang}:")
+  fact_messages[lang] = {}
+  item_messages[lang] = {}
+  # Extract the comments:
+  for name in ['Fish', 'Insect', 'Fossil']:
+    fact_messages[lang][name] = msbt.parse_file(f"{cwd}/extracted/{lang}/SP_owl_Comment_{name}.msbt", verbose=True)
 
-# Extract the names:
-for n, name in [('31', 'Fish'), ('30', 'Insect'), ('34', 'Fossil')]:
-  item_messages[name] = msbt.parse_file(f"{cwd}/Extracted/STR_ItemName_{n}_{name}.msbt", verbose=True)
+  # Extract the names:
+  for n, name in [('31', 'Fish'), ('30', 'Insect'), ('34', 'Fossil')]:
+    item_messages[lang][name] = msbt.parse_file(f"{cwd}/extracted/{lang}/STR_ItemName_{n}_{name}.msbt", verbose=True)
+
+  print("\n", end='')
 
 # Tidy up data a little bit:
-for dictionary in [ fact_messages, item_messages ]:
-  for itemtype, data in dict.items(dictionary):    
-    data['attributes'] = [ val.strip('\x00') for val in data['attributes'] ]
-    data['attributes'] = [ val for val in data['attributes'] if val != '' ]
+for subset in [ fact_messages, item_messages ]:
+  for lang, messages in dict.items(subset):
+    for name, data in dict.items(messages):
+      data['attributes'] = [ val.strip('\x00') for val in data['attributes'] ]
+      data['attributes'] = [ val for val in data['attributes'] if val != '' ]
 
-export(all_styles, f"{cwd}/Output/styles.json")
-export(fact_messages, f"{cwd}/Output/comments.json")
-export(item_messages, f"{cwd}/Output/names.json")
+export(all_styles, f"{cwd}/output/styles.json")
+export(fact_messages, f"{cwd}/output/comments.json")
+export(item_messages, f"{cwd}/output/names.json")
